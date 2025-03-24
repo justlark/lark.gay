a9fb2a7ae44a897fa981862a7fb05ddf3115af59
Lark Aster <lark@lark.gay>
2025-03-24 03:28:19 -0400

    Fix bug in Pages function

--- static/_worker.js
+++ static/_worker.js
@@ -6,13 +6,11 @@ export default {
     const url = new URL(request.url);
 
     if (PROXY_PREFIXES.some((prefix) => url.pathname.startsWith(prefix))) {
-      if (url.pathname.startsWith(prefix)) {
-        return await fetch(`https://${PROXY_DOMAIN}${url.pathname}`, {
-          method: request.method,
-          headers: request.headers,
-          body: request.body,
-        });
-      }
+      return await fetch(`https://${PROXY_DOMAIN}${url.pathname}`, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+      });
     }
 
     return env.ASSETS.fetch(request);
