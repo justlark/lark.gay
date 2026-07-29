7a731e9ba7c39016a3413bfd6ffa58264780bec3
Lark Aster <lark@lark.gay>
2026-07-29 04:51:58 -0400

    Update flipboard message page

--- site/static/assets/flipboard.js
+++ site/static/assets/flipboard.js
@@ -1,15 +1,22 @@
 const inputTextarea = document.getElementById("flipboard-input");
 const sendButton = document.getElementById("flipboard-send-button");
+const warningText = document.getElementById("flipboard-warning");
 
-sendButton.addEventListener("click", () => {
+sendButton.addEventListener("click", async () => {
   const message = inputTextarea.value.trim();
   const params = new URLSearchParams(location.search);
 
   if (message) {
-    fetch(`/flipboard/?token=${params.get("token")}`, {
+    let response = await fetch(`/flipboard/?token=${params.get("token")}`, {
       method: "POST",
       body: message,
     });
+
+    if (response.status === 403) {
+      warningText.hidden = false;
+    } else {
+      warningText.hidden = true;
+    }
 
     inputTextarea.value = "";
   }
