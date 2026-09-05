cff9e5f0a40a35e45dcce4e5a56ccd752cd220ed
Lark Aster <lark@lark.gay>
2026-09-05 15:01:49 -0400

    Add tinylog post page

---
+++ site/content/tinylog.md
@@ -0,0 +1,5 @@
++++
+title = "Post a tinylog entry"
+description = "Post a message to Lark's mission log in gemspace."
+template = "tinylog.html"
++++
---
+++ site/static/assets/tinylog.js
@@ -0,0 +1,45 @@
+const inputTextarea = document.getElementById("flipboard-input");
+const sendButton = document.getElementById("flipboard-send-button");
+const authWarningText = document.getElementById("flipboard-permission-warning");
+const successText = document.getElementById("flipboard-success");
+
+const TOAST_TTL_MILLIS = 5000;
+
+sendButton.addEventListener("click", async () => {
+  const message = inputTextarea.value.trim();
+  const params = new URLSearchParams(location.search);
+
+  if (message) {
+    let response = await fetch(`https://tinylog-bot.lark.gay/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${params.get("token")}`,
+      },
+      body: JSON.stringify({
+        message: message,
+      }),
+    });
+
+    if (response.status === 401 || response.status === 403) {
+      authWarningText.hidden = false;
+
+      setTimeout(() => {
+        authWarningText.hidden = true;
+      }, TOAST_TTL_MILLIS);
+    } else {
+      authWarningText.hidden = true;
+    }
+
+    if (response.status === 200) {
+      successText.hidden = false;
+
+      setTimeout(() => {
+        successText.hidden = true;
+      }, TOAST_TTL_MILLIS);
+    } else {
+      successText.hidden = true;
+    }
+
+    inputTextarea.value = "";
+  }
+});
